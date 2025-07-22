100fe7ba7c0657450ef82181bfa35e43c3d4f598
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2025-07-23 00:48:26 +0530

    adding login and logout

--- task-app/db/mongoose.js
+++ task-app/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://aryamansharma1816:yw8hPcNljZm1WpMw@cluster0.yfdapul.mongodb.net/task_app?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb://aryamansharma1816:yw8hPcNljZm1WpMw@ac-ihp1kwz-shard-00-00.yfdapul.mongodb.net:27017,ac-ihp1kwz-shard-00-01.yfdapul.mongodb.net:27017,ac-ihp1kwz-shard-00-02.yfdapul.mongodb.net:27017/task-app?ssl=true&replicaSet=atlas-bwz2fv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
