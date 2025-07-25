432670835dccf4f82ebb4008a6ef801d2405efd8
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2025-07-25 16:19:14 +0530

    adding CRUD operations for task

--- task-app-mine/controllers/task-controller.js
+++ task-app-mine/controllers/task-controller.js
@@ -2,8 +2,9 @@ const Task = require('../models/task-model');
 
 exports.getAllTasks = async (req, res) => {
     try {
+        const tasks = await Task.find();
         res.status(200).json({
-            message: 'route not yet defined'
+            data: tasks
         });
     } catch (err) {
         es.send(500).json({
@@ -15,6 +16,55 @@ exports.getAllTasks = async (req, res) => {
 exports.createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body);
+        res.status(200).json({
+            data: task
+        });
+    } catch (err) {
+        res.send(500).json({
+            message: 'Something went wrong'
+        });
+    }
+}
+
+exports.getTask = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const task = await Task.findById(id);
+        res.status(200).json({
+            data: task
+        });
+    } catch (err) {
+        res.send(500).json({
+            message: 'Something went wrong'
+        });
+    }
+}
+
+exports.updateTask = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const task = await Task.findByIdAndUpdate(id, req.body, {
+            runValidators: true,
+            new: true
+        });
+        res.status(200).json({
+            success: 'SUCCESS',
+            data: task
+        });
+    } catch (err) {
+        res.send(500).json({
+            success: 'FAILURE',
+            message: 'Something went wrong'
+        });
+    }
+
+
+}
+
+exports.deleteTask = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const task = await Task.findByIdAndDelete(id);
         res.status(200).json({
             data: task
         });
--- task-app-mine/routes/task-router.js
+++ task-app-mine/routes/task-router.js
@@ -3,5 +3,6 @@ const router = new express.Router();
 const taskController = require('../controllers/task-controller');
 
 router.route('/').get(taskController.getAllTasks).post(taskController.createTask);
+router.route('/:id').get(taskController.getTask).delete(taskController.deleteTask).patch(taskController.updateTask);
 
 module.exports = router;
